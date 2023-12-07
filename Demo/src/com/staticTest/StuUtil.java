e74eca250364bd4c86e457ed835443f674c824b8
huangzz <huangzheng326@gmail.com>
2023-12-07 21:27:16 +0800

    工具类测试案例添加,增加学习笔记

--- Demo/src/com/staticTest/StuUtil.java
+++ Demo/src/com/staticTest/StuUtil.java
@@ -3,11 +3,11 @@ package com.staticTest;
 import java.util.ArrayList;
 
 public class StuUtil {
+
     private StuUtil() {
     }
 
     public static int getMaxAge(ArrayList<Students> arr) {
-
         int max = arr.get(0).getAge();
 
         for (int i = 0; i < arr.size(); i++) {
